039c120aeec84c4ee33c1c52bfb073567a685d73
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-05-08 17:19:43 +0300

    Added General Feed Sorting.

--- SaADP2_3_One_file/Main_2_3.cpp
+++ SaADP2_3_One_file/Main_2_3.cpp
@@ -7,24 +7,86 @@ struct Array
 {
 	int key;
 };
-enum sorting 
+
+struct ListItem
+{
+	int data;
+	ListItem* next;
+};
+
+enum sorting
 {
 	MinNumber = 0, MaxNumber = 250,
-	MinSize = 1, MaxSize = 10000
+	MinSize = 1, MaxSize = 10000, MinKey = 0, MaxKey = 9999
 };
-enum userInterface 
-{ 
-	ShowMenu, SimpleFeedSort, FeedSort, Exit,
+enum userInterface
+{
+	ShowMenu, SimpleFeedSort, FeedSort, GeneralFeedSort, Exit,
 	LineWidth = 20
 };
 
+/*   LIST   */
+void addItem(ListItem*& pHead, int data)
+{
+	ListItem* pCurrent = pHead;
+	while (pCurrent->next != nullptr)
+	{
+		pCurrent = pCurrent->next;
+	}
+	pCurrent->next = new ListItem;
+	pCurrent->next->data = data;
+	pCurrent->next->next = nullptr;
+}
+
+void clearMemoryList(ListItem* currentArray[], int size)
+{
+	ListItem* pCurrent ;
+	for (int i = 0; i < size; i++)
+	{
+		pCurrent = currentArray[i]->next;
+		while (pCurrent != nullptr)
+		{
+			ListItem* pTemp = pCurrent;
+			pCurrent = pCurrent->next;
+			delete pTemp;
+		}
+		pCurrent = nullptr;
+	}
+	delete[] currentArray; currentArray = nullptr;
+}
+
+void showArrayOfList(ListItem* currentArray[], int size)
+{
+	ListItem* pCurrent; int steps;
+	for (int i = 0; i < size; i++)
+	{
+		steps = 1;
+		pCurrent = currentArray[i]->next;
+		while (pCurrent != nullptr)
+		{
+			if (steps % LineWidth == 0) { cout << "\n"; }
+			cout << pCurrent->data << "   ";
+			pCurrent = pCurrent->next;
+		}
+	}
+}
 /*   SORTING   METHODS   */
 
 int random(int max) { return MinNumber + rand() % (max - MinNumber + 1); }
 
+void fillRandomArray(Array* currentArray, int size, int maxKey)
+{
+	for (int cell = 0; cell < size; cell++)
+	{
+		currentArray[cell].key = random(maxKey);
+	}
+}
 void fillSortedArray(Array* currentArray, int size)
 {
-	for (int cell = 0; cell < size; cell++) { currentArray[cell].key = cell;	}
+	for (int cell = 0; cell < size; cell++) 
+	{ 
+		currentArray[cell].key = cell;	
+	}
 }
 
 void shuffleArray(Array* currentArray, int size)
@@ -86,6 +148,15 @@ void feedSort(Array* mainArray, Array* auxArray, int& countExchanges, int size)
 	}
 }
 
+void generalFeedSort(ListItem* auxArray[], Array* currentArray, int& countExchanges, int size)
+{	
+	for (int i = 0; i < size; i++)
+	{
+		addItem(auxArray[currentArray[i].key], currentArray[i].key);
+		countExchanges++;
+	}
+}
+
 /*   INTERFACE METHODS   */
 
 int input()
@@ -99,11 +170,11 @@ int input()
 	return number;
 }
 
-int sizeInput()
+int sizeInput(int minValue, int maxValue)
 {
-	int size = 0;
-	while (size < MinSize || size > MaxSize) { cout << "   Enter the size (1 - 10000): "; size = input(); }
-	return size;
+	int value = 0;
+	while (value < minValue || value > maxValue) { cout << "   Enter: "; value = input(); }
+	return value;
 }
 
 void printMenu()
@@ -115,7 +186,7 @@ void printMenu()
 		<< "\n4. Exit." << endl;
 }
 
-void showInfo(Array* mainArray, Array* auxArray, int& countCompares, int& countExchanges, int size)
+void showInfo(Array* mainArray, Array* auxArray, int countCompares, int countExchanges, int size)
 {
 	cout << "\n   Before sorting:\n   ";
 	show(mainArray, size);
@@ -124,8 +195,18 @@ void showInfo(Array* mainArray, Array* auxArray, int& countCompares, int& countE
 	cout << "\n   Number of comparisons: " << countCompares << "\n\n   Number of exchanges: " << countExchanges << "\n\n";
 }
 
-void caseSimpleFeedSort(int size)
+void showInfoGeneralFeed(Array* mainArray, ListItem* auxArray, int countCompares, int countExchanges, int size)
 {
+	cout << "\n   Before sorting:\n   ";
+	show(mainArray, size);
+	cout << "\n\n   After sorting:\n   ";
+	showArrayOfList(&auxArray, size);
+	cout << "\n   Number of comparisons: " << countCompares << "\n\n   Number of exchanges: " << countExchanges << "\n\n";
+}
+
+void caseSimpleFeedSort()
+{
+	cout << "   Enter the maximum array size (> 0, < 10 000)\n"; int size = sizeInput(MinSize, MaxSize);
 	int countCompares{ 0 }, countExchanges{ 0 };
 	Array* mainArray = new Array[size];
 	Array* auxArray = new Array[size];
@@ -138,8 +219,9 @@ void caseSimpleFeedSort(int size)
 	clearMemory(auxArray, size);
 }
 
-void caseFeedSort(int size)
+void caseFeedSort()
 {
+	cout << "   Enter the maximum array size (> 0, < 10 000)\n"; int size = sizeInput(MinSize, MaxSize);
 	int countCompares{ 0 }, countExchanges{ 0 };
 	Array* mainArray = new Array[size];
 	Array* auxArray = new Array[size];
@@ -151,9 +233,26 @@ void caseFeedSort(int size)
 	clearMemory(auxArray, size);
 }
 
+void caseGeneralFeedSort()
+{
+	cout << "   Enter the maximum key value (> 0, < 9 999)\n"; int maxKey = sizeInput(MinKey, MaxKey);
+	cout << "   Enter the maximum array size (> maxKey, < 10 000)\n"; int size = sizeInput(maxKey, MaxSize);
+	int countCompares{ 0 }, countExchanges{ 0 };
+	Array* mainArray = new Array[size];
+	fillRandomArray(mainArray, size, maxKey);
+
+	ListItem* auxArray = new ListItem[MaxKey];
+	for (int cell = 0; cell < maxKey; cell++)
+	{
+		auxArray[cell].next = nullptr;
+	}
+	generalFeedSort(&auxArray, mainArray, countExchanges, size);
+	showInfoGeneralFeed(mainArray, auxArray, countCompares, countExchanges, size);
+	clearMemory(mainArray, size);
+	clearMemoryList(&auxArray, maxKey);
+}
 void userInterface()
 {
-	int size = sizeInput();
 	bool stop = false; printMenu();
 
 	while (!stop)
@@ -161,11 +260,12 @@ void userInterface()
 		cout << "   Choice: ";
 		switch (input())
 		{
-		case ShowMenu:       { printMenu(); break; }
-		case SimpleFeedSort: { caseSimpleFeedSort(size); break; }
-		case FeedSort:       { caseFeedSort(size); break; }
-		case Exit:           { stop = true; break; }
-		default:               cout << "   There is no such menu item.\n\n"; break;
+		case ShowMenu:        { printMenu(); break; }
+		case SimpleFeedSort:  { caseSimpleFeedSort(); break; }
+		case FeedSort:        { caseFeedSort(); break; }
+		case GeneralFeedSort: { caseGeneralFeedSort(); break; }
+		case Exit:            { stop = true; break; }
+		default:                cout << "   There is no such menu item.\n\n"; break;
 		}
 		cout << "   (0) - show menu.\n";
 	}
